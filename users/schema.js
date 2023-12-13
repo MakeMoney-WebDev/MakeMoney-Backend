e1ae80a6c21a2dbe0aa6298f1e7d3b6260dc3ef9
entran02 <evanntran2002@gmail.com>
2023-12-13 18:29:39 -0500

    add user/admin role

--- users/schema.js
+++ users/schema.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "Watchlist",
 		},
+		role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user'
+        }
 	},
 	{ collection: "users" }
 );
